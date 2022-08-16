f8c226b1e92f6345f0d43b4b94fd984f030db7f9
Christian Beckham <urenacb22@gmail.com>
2022-08-16 11:30:14 -0500

    Created separate Lists with sample data

---
+++ main.py
@@ -0,0 +1,4 @@
+destinations = ['Green Bay', 'Madison', 'Milwaukee']
+restaurants = ['Burger King', 'Culvers', 'Kopps', 'McDonalds', 'Shake Shack']
+types_of_transportation = ['Car', 'Taxi', 'Bus', 'Airplane', 'Train']
+types_of_entertainment = ['Amusement park', 'Concert', 'Fair', 'Movie', 'Museum', 'Sporting event']
