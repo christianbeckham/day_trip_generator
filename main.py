c5c33add2dde8a1ed2606d82d22b9c28e31608b8
Christian Beckham <urenacb22@gmail.com>
2022-08-16 11:41:47 -0500

    Implemented function to make random selection

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from random import randrange
+
 destinations = ['Green Bay', 'Madison', 'Milwaukee']
 restaurants = ['Burger King', 'Culvers', 'Kopps', 'McDonalds', 'Shake Shack']
 types_of_transportation = ['Car', 'Taxi', 'Bus', 'Airplane', 'Train']
-types_of_entertainment = ['Amusement park', 'Concert', 'Fair', 'Movie', 'Museum', 'Sporting event']
+types_of_entertainment = ['Amusement park', 'Concert',
+                          'Fair', 'Movie', 'Museum', 'Sporting event']
+
+
+def make_random_selection(list):
+    random_index = randrange(0, len(list))
+    random_item = list[random_index]
+    return random_item
+
+
+random_destination = make_random_selection(destinations)
+print(random_destination)
